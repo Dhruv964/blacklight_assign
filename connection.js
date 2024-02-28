bb905a4fe8d9bb4a26f104cfc6519e01cffc34ca
krishchopra02 <krish.ug20@nsut.ac.in>
2024-02-28 15:55:52 +0530

    Added sequelize

--- connection.js
+++ connection.js
@@ -1,11 +1,13 @@
-import mysql from 'mysql2/promise';
+import Sequelize from 'sequelize'
 import 'dotenv/config'
-const pool = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_DATABASE || 'test',
-  connectTimeout: 1000000
-});
-
-export default pool;
+const URI = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URI}/${process.env.DB_NAME}`
+const sequelize = new Sequelize(URI)
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection to db has been established successfully')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+export default { connectDB, sequelize }
