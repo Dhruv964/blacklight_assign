b953f994d45d8ee05c8bb3e3f36f47ae052eaf2b
Dhruv964 <91016974+Dhruv964@users.noreply.github.com>
2024-02-28 15:31:35 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ const pool = mysql.createConnection({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_DATABASE || 'test',
-  connectTimeout: 10000
+  connectTimeout: 1000000
 });
 
 export default pool;
