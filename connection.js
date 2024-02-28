78ebe94fa6df36a0f723976c98cd0b446add4d81
Dhruv964 <91016974+Dhruv964@users.noreply.github.com>
2024-02-28 15:18:05 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,18 +1,11 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config'
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_DATABASE || 'test',
-  waitForConnections: true,
-  connectionLimit: 100,
-  acquireTimeout: 10000,
-  maxIdle: 10,
-  idleTimeout: 600000,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+  connectTimeout: 10000
 });
 
 export default pool;
